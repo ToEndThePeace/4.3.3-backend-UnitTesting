6ec656211f2dd3174e7a27f7db96f9b49bb7e71a
unknown <bran.ramirez.don@gmail.com>
2020-06-17 11:48:35 -0700

    Initial commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,45 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  /**
+   * - accepts an item
+   * - returns a new item object that has
+   *   been modified successfully
+   * - on success:
+   *    - increase enhancement by 1
+   *    - if enhancement is 20, don't change it
+   *    - don't change durability
+   */
+  return item
+    ? // Item was passed in
+      {
+        ...item,
+        enhancement:
+          item.enhancement < 20 ? item.enhancement + 1 : item.enhancement,
+      }
+    : // Item was not passed in
+      {};
 }
 
 function fail(item) {
+  /**
+   * - accepts an item
+   * - returns a new item that failed enhancement
+   * - on failure:
+   *    - if enhancement < 15, durability -= 5
+   *    - if enhancement >= 15, durability -= 10
+   *    - if enhancement > 16, enhancement -= 1
+   */
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  /**
+   * - accepts an item
+   * - returns a new item with durability 100
+   */
+  return item ? { ...item, durability: 100 } : {};
+  // return item;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
+
+const items = [
+  {
+    name: "Pickaxe",
+    durability: 100,
+    enhancement: 0,
+  },
+  {
+    name: "Broadsword",
+    durability: 50,
+    enhancement: 10,
+  },
+  {
+    name: "Longbow",
+    durability: 0,
+    enhancement: 20,
+  },
+];
+
+describe("enhancer", () => {
+  describe("repair", () => {
+    it("should return something", () => {
+      expect(repair()).not.toBeNull;
+    });
+    it("should return a new object", () => {
+      expect(repair(items[2])).not.toBe(items[2]);
+    });
+    it("should return an item with durability 100", () => {
+      expect(repair(items[2]).durability).toBe(100);
+    })
+  });
+  describe("succeed", () => {
+    it("should return something", () => {
+      expect(succeed()).not.toBeNull;
+    });
+  });
+  describe("fail", () => {
+    it("should return something", () => {
+      expect(fail()).not.toBeNull;
+    });
+  });
+  describe("get", () => {
+    it("should return something", () => {
+      expect(get()).not.toBeNull;
+    });
+  });
+});
